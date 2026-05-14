38bfff73f911cd844eec73b81fc56fe6624c0ade
Kevin Allen <kevin11.allen@nttdata.com>
2026-05-14 13:43:10 -0400

    update grafana name validation

--- variables.tf
+++ variables.tf
@@ -107,13 +107,8 @@ variable "grafana_name" {
   default     = null
 
   validation {
-    condition     = can(regex("^[a-z][a-z0-9-]+[a-z0-9]$", var.grafana_name))
-    error_message = "The name can only contain lowercase letters, numbers and dashes, and it must begin with a letter and end with a letter or digit."
-  }
-
-  validation {
-    condition     = length(var.grafana_name) < 23 && length(var.grafana_name) > 2
-    error_message = "The name length must be from 2 to 23 characters"
+    condition     = can(regex("^[a-zA-Z][a-z0-9A-Z-]{0,28}[a-z0-9A-Z]$", var.grafana_name))
+    error_message = "The name must be 2-30 characters, start with a letter, end with a letter or digit, and contain only letters, numbers, and hyphens."
   }
 }
 
